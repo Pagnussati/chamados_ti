38e69de3845c0a0ca7d94feff30084f327830809
Juaum <127996526+Pagnussati@users.noreply.github.com>
2024-10-08 10:46:23 -0300

    organizando codigo criar usuario

--- frontend/pages/signin.php
+++ frontend/pages/signin.php
@@ -142,6 +142,42 @@
       });
     });
 
+    // Funcao de verificar idade
+    function verifyAge(birthDate) {
+      var parts = birthDate.split('/');
+      var birthDateObj = new Date(parts[2], parts[1] - 1, parts[0]); // ano, mês, dia
+
+      var today = new Date();
+      var age = today.getFullYear() - birthDateObj.getFullYear();
+      var m = today.getMonth() - birthDateObj.getMonth();
+
+      if (m < 0 || (m === 0 && today.getDate() < birthDateObj.getDate())) {
+        age--;
+      }
+
+      if (age < 18) {
+        $('#adultWarn').text('Você precisa ter pelo menos 18 anos para se cadastrar.');
+        return false;
+      }
+
+      $('#adultWarn').text(''); // Limpa o aviso caso a idade seja válida
+      return true;
+    }
+
+    // Funcao de verificar senha
+    function verifyPassword(password, confirmPassword) {
+      if (password.length < 4) {
+        $('#passwordWarn').text('A senha precisa ter pelo menos 4 caracteres!');
+        return false;
+      } else if (password !== confirmPassword) {
+        $('#passwordWarn').text('As senhas não coincidem!');
+        return false;
+      }
+
+      $('#passwordWarn').text(''); // Limpa o aviso caso as senhas sejam válidas
+      return true;
+    }
+
     // Verificando antes de enviar para o backend
     $('#createUserForm').submit((e) => {
       e.preventDefault();
@@ -156,33 +192,12 @@
       var password = $('#password').val();
       var confirmPassword = $('#passwordConfirm').val();
 
-      // Verificando idade
-      var parts = birthDate.split('/');
-      var birthDateFormatted = `${parts[2]}-${parts[1]}-${parts[0]}`; // Data no formato do SQL para salvar no banco
-      var birthDateObj = new Date(parts[2], parts[1] - 1, parts[0]); // ano, mês, dia
+      var validAge = verifyAge(birthDate);
+      var validPassword = verifyPassword(password, confirmPassword);
 
-      var today = new Date();
-      var age = today.getFullYear() - birthDateObj.getFullYear();
-      var m = today.getMonth() - birthDateObj.getMonth();
-      if (m < 0 || (m === 0 && today.getDate() < birthDateObj.getDate())) {
-        age--;
-      }
-
-      if (age < 18) {
-        $('#adultWarn').text('Você precisa ter pelo menos 18 anos para se cadastrar.');
-        e.preventDefault();
+      if (!validAge || !validPassword) {
         return;
-      };
-      if ((password && confirmPassword).length < 4) {
-        $('#passwordWarn').text('A senha precisa ter 4 caracteres!');
-        e.preventDefault();
-        return
-      };
-      if (!(password === confirmPassword)) {
-        $('#passwordWarn').text('As senhas não coincidem!');
-        e.preventDefault();
-        return
-      };
+      }
 
       var formData = {
         name: name,
